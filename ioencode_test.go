198c5c28ad0cffdb5a936394a23b3ec40721037b
Warren Hodgkinson <warren.hodgkinson@jet.com>
2020-03-02 08:48:48 +0000

    Added TestReader

--- ioencode_test.go
+++ ioencode_test.go
@@ -14,3 +14,24 @@ func (w *TestWriter) Write(p []byte) (n int, err error) {
 
   return len(p), nil
 }
+
+type TestReader struct {
+  buffer []byte
+  err error
+}
+
+func (r *TestReader) Read(p []byte) (n int, err error) {
+  if r.err != nil {
+    return 0, r.err
+  }
+
+  toCopy := len(p)
+  if toCopy > len(r.buffer) {
+    toCopy = len(r.buffer)
+  }
+
+  copy(p, r.buffer)
+  r.buffer = r.buffer[toCopy:]
+
+  return toCopy, nil
+}
